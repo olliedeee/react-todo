321ad3454dabd9ec27d8ebf4ebf531eb765539a7
Ollie <oliverdhillon@gmail.com>
2023-04-17 00:36:52 +0100

    add uiud to set unique keys, add Todos component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+
+  const todoNameRef = useRef()
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null;
+
+  }
   return (
     <>
-    < TodoList />
-    <input type="test"></input>
-    <button>Add Todo</button>
+    < TodoList todos={todos}/>
+    <input ref={todoNameRef} type="test"></input>
+    <button onClick={handleAddTodo}>Add Todo</button>
     <button>Clear Completed Todos</button>
-    <div>0 Todos remaining</div>
+    <div>{todos.count} Todos remaining</div>
     </>
   )
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo';
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
   return (
-    <div>TodoList</div>
-  )
+    <div>
+      <h1>Todos</h1>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} />;
+      })}
+    </div>
+  );
 }
