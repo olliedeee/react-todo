e313670736ace10b9fef89e53dd920e83277b0e9
Ollie <oliverdhillon@gmail.com>
2023-04-17 01:05:30 +0100

    add toggling of Todo Status and persist Todos in Local Storage

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Todo from './Todo';
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, toggleTodo}) {
   return (
     <div>
       <h1>Todos</h1>
       {todos.map(todo => {
-        return <Todo key={todo.id} todo={todo} />;
+        return <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} />;
       })}
     </div>
   );
